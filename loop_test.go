65d24e5e1e8a5bb194e59eecb17007cb04b87d73
Daniël de Kok <me@danieldk.eu>
2012-10-03 20:25:59 +0200

    Add loop tests.
    They where already implicitly checked by the map tests.

---
+++ loop_test.go
@@ -0,0 +1,37 @@
+package par
+
+import (
+	"testing"
+)
+
+func makeBoolData(n uint) []bool {
+	return make([]bool, n)
+}
+
+func verifyAllTrue(t *testing.T, name string, data []bool) {
+	for idx, val := range data {
+		if !val {
+			t.Errorf("%s[%d] == false, expected true", name, idx)
+		}
+	}
+}
+
+func genericForTest(t *testing.T, name string, loop ParallelForLoop) {
+	for i := uint(1); i < maxsize; i *= 2 {
+		data := makeBoolData(i)
+
+		ForInterleaved(0, len(data), func(idx int) {
+			data[idx] = true
+		})
+
+		verifyAllTrue(t, name, data)
+	}
+}
+
+func TestForChunked(t *testing.T) {
+	genericForTest(t, "TestForInterleaved", ForChunked)
+}
+
+func TestForInterleaved(t *testing.T) {
+	genericForTest(t, "TestForInterleaved", ForInterleaved)
+}
