49dc6175ca9635ae5e93e8ed9de840bffd2d4706
Daniël de Kok <me@danieldk.eu>
2012-10-03 11:06:11 +0200

    Remove useless ParMap function.

--- map.go
+++ map.go
@@ -1,27 +1,5 @@
 package par
 
-// Simple parallel map, that starts a Goroutine for every value.
-// Do not use for large vectors, the Goroutines will eat all your
-// memory!
-func ParMap(f func(float64) float64, l []float64) []float64 {
-	n := len(l)
-	result := make([]float64, n)
-	sem := make(semaphore, n)
-
-	for i := 0; i < n; i++ {
-		go func(idx int) {
-			result[idx] = f(l[idx])
-			sem <- empty{}
-		}(i)
-	}
-
-	for i := 0; i < n; i++ {
-		<-sem
-	}
-
-	return result
-}
-
 // Map over a slice of float64s with the given parallel for-loop.
 func MapFloat64(loop ParallelForLoop, f func(float64) float64, l []float64) []float64 {
 	result := make([]float64, len(l))
